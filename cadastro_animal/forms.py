0202b348ebc392632f9f77c0fc46751ce0aa723b
Andrezza Nogueira <andrezza.n.dasilva@gmail.com>
2024-10-28 17:37:37 -0300

    melhorias na aparencia dos formularios e cricao do app cadastro_usuario

--- cadastro_animal/forms.py
+++ cadastro_animal/forms.py
@@ -1,7 +1,21 @@
 from django import forms
+
 from .models import CadastroAnimal, GaleriaAnimal
+
 
 class FormCadastroAnimal(forms.ModelForm):
     class Meta:
         model = CadastroAnimal
-        fields = "__all__"
+        fields = ['nome', 'idade', 'sexo', 'especie']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Digite o nome do animal'}),
+            'idade': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Idade do animal'}),
+            'sexo': forms.Select(attrs={'class': 'form-select'}),
+            'especie': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+
+class GaleriaAnimalForm(forms.ModelForm):
+    class Meta:
+        model = GaleriaAnimal
+        fields = ['imagem']
--- cadastro_animal/urls.py
+++ cadastro_animal/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("cadastro-animal/", views.cadastro, name="cadastro_animal"),
     path("lista-animais/", views.lista_animais, name="lista_animais"),
-    path('animal/<int:animal_id>/', views.detalhe_animal, name='detalhe_animal'),
-    path("pesquisar-animais/", views.pesquisar_animais, name="pesquisar_animais")
+    path('animal/<int:animal_id>/', views.detalhe_animal, name='detalhe_animal')
 ]
