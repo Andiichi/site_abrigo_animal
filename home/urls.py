62ad951a1d57c67194c21034f8954dd9ceaa30e8
Andiichi <andrezza.n.dasilva@gmail.com>
2024-11-06 20:52:18 -0300

    correcao da urls do app home

--- home/urls.py
+++ home/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from . import views
+from cadastro_animal.views import lista_animais
 
 app_name = 'home'
 
 urlpatterns = [
     path("", views.lista_animais, name="home"),
     #pagina de pesquisa do cadastro_animais
-    path("pesquisar-animais/", pesquisar_animais, name="pesquisar_animais"),
+    path("pesquisar-animais/", views.pesquisar_animais, name="pesquisar_animais"),
     # URL para a página estática
     path('sobre/', TemplateView.as_view(template_name='sobre.html'), name='sobre'),
     path('contato/', TemplateView.as_view(template_name='contato.html'), name='contato'),
